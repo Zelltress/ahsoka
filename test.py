f37ed0b356a1ee7c3d726ff523a2463efee31202
Zelltress <showofthesoul@gmail.com>
2024-02-25 19:12:56 +0700

    some code_1

--- test.py
+++ test.py
@@ -1,52 +1,79 @@
+import ru_local as ru
 import matplotlib.pyplot as plt
-import numpy as np
 
-a = float(input())
-b = float(input())
-c = float(input())
-d = float(input())
-pr = float(input())
-k = 0
-idx = 0
-pr_e = 200
-q_e = 100
-prices_0 = []
-prices_1 = []
-products = []
-
+a = float(input('функция вида QD = a-bp, где a - свободный член. (Количество людей, которые хотят купить данный товар) '))
+b = float(input('функция вида QD = a-bp, где b - коэф. наклона (Он всегда отрицателен, поэтому запишите положительное число. т.е Если a - 6p, то запишите просто 6) '))
+c = float(input('функция вида QS = c-dp, где c - свободный член. (Количество людей, которые хотят продавать данный товар) '))
+t = float(input('функция вида QS = c-dp, где t - коэф. наклона (Он всегда положителен)'))
+price = float(input())
+prices = []
+demands = []
+supplies = []
+i = 0 #счетчик
 
 def demand(p):
-    return a - b * p
+    return a-b*p
 
 
 def supply(p):
-    return c + d * p
+    return c+t*p
 
+def main():
+    price_e = (a-c)/(t+b)
+    quantity_e = demand(price_e)
 
-if d > b:
-    print('Модель с раскручивающейся спиралью')  # т.е.колебания растут
-elif d == b:
-    print('Модель с постоянными колебаниями')
-else:
-    print('Модель с закручивающейся спиралью')  # d<b колебания затухают
+    print('Равновесная цена:', price_e, 'Равновесный объём:', quantity_e)
 
-if pr == pr_e:
-    print('Модель находится в равновесии')
-    print(f'{k} циклов понаобится')
-else:
-    for i in range(0, 1000):
-        prices_0.append(supply(pr))
-        print(prices_0)
-        idx = (idx + 1) % len(prices_0)
-        print(idx)
-        prices_1.append(demand(prices_0[idx]))
-        k += 1
-        if pr in prices_0 or prices_1:
-            print('Модель находится в равновесии')
-            print(f'{k} циклов понаобится')
-            break
+    if price == price_e:
+        print('Модель находится в равновесии')
+
+    else:
+        print('Модель не находится в равновесии')
+
+        if t == b:
+            print('Модель с постоянными колебаниями')
+            for j in range(2):
+                demands.append(supply(price))
+                price = (a - c - t*price)/b
+                supplies.append(supply(price))
+                prices.append(price)
+
+        elif t > b:
+            print('Модель с раскручивающейся спиралью')
+            for k in range(5):
+                demands.append(supply(price))
+                price = (a - c - t*price)/b
+                supplies.append(supply(price))
+                prices.append(price)
         else:
-            continue
-fig, ax = plt.subplots()
-ax.set_xlabel('Q')
-ax.set_ylabel('P')
+            print('Модель с закручивающейся спиралью')
+            while price != price_e:
+                i += 1
+                demands.append(supply(price))
+                price = (a - c - t*price)/b
+                supplies.append(supply(price))
+                prices.append(price)
+
+        print(f'{i} циклов(a) понадобится')
+
+    plt.figure()
+    plt.title('Паутинообразная модель динамики цен и объёма производства')
+    plt.ylabel('Цена товара')
+    plt.xlabel('Количество товара')
+    plt.plot(demands, prices)
+    plt.plot(supplies, prices)
+    plt.scatter(demands, prices)
+    plt.scatter(supplies, prices)
+
+    for m in demands:
+        for n in supplies:
+            if m == n:
+                plt.quiver(n, prices[supplies.index(n)], 0, prices[demands.index(m)]-prices[supplies.index(n)],
+                       angles='xy', scale_units='xy', scale=1, width=0.005) #рисуем вектор
+    for q in prices:
+        plt.quiver(demands[prices.index(q)], q, supplies[prices.index(q)]-demands[prices.index(q)], 0,
+               angles='xy', scale_units='xy', scale=1, width=0.005)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
