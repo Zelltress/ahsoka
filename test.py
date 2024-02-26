e4ce0b392e7be8608547562c3b17f30e3cea5d23
Zelltress <showofthesoul@gmail.com>
2024-02-26 20:00:23 +0700

    add 1.6

--- test.py
+++ test.py
@@ -27,70 +27,80 @@ def main():
     i = 0
     price_begin = -1
 
-    price_e = (a - c) / (t + b)
-    quantity_e = demand(price_e)
+    if t == 0 and b == 0:
 
-    print(ru.EQUILIBRIUM_PRICE, price_e, ru.EQUILIBRIUM_VOLUME, quantity_e)
-
-    if price == price_e:
-        print(ru.BALANCE_MODEL)
-        for d in range(100, 300):
-            demands.append(demand(d))
-            supplies.append(supply(d))
-            prices.append(d)
-            price_begin = price
+        if a == c:
+            print('Графики совпадают')
+        else:
+            print('Графики не пересекаются')
+    elif t+b == 0:
+        print('Нарушение закона спроса или закона предложения')
 
     else:
-        print(ru.IMBALANCE_MODEL)
+        price_e = (a - c) / (t + b)
+        quantity_e = demand(price_e)
 
-        if t == b:
-            print(ru.CONSTANT_CYCLE)
-            for j in range(2):
-                demands.append(supply(price))
-                price = (a - c - t * price) / b
-                supplies.append(supply(price))
-                prices.append(price)
+        print(ru.EQUILIBRIUM_PRICE, price_e, ru.EQUILIBRIUM_VOLUME, quantity_e)
 
-        elif t > b:
-            print(ru.UNWINDING_SPIRAL)
-            for k in range(5):
-                demands.append(supply(price))
-                price = (a - c - t * price) / b
-                supplies.append(supply(price))
-                prices.append(price)
+        if price == price_e:
+            print(ru.BALANCE_MODEL)
+            for d in range(100, 300):
+                demands.append(demand(d))
+                supplies.append(supply(d))
+                prices.append(d)
+                price_begin = price
+
         else:
-            print(ru.TWISTING_SPIRAL)
-            while price != price_e:
-                i += 1
-                demands.append(supply(price))
-                price = (a - c - t * price) / b
-                supplies.append(supply(price))
-                prices.append(price)
+            print(ru.IMBALANCE_MODEL)
 
-        print(i, ru.NUMBER_CYCLES)
+            if t == b:
+                print(ru.CONSTANT_CYCLE)
+                for j in range(2):
+                    demands.append(supply(price))
+                    price = (a - c - t * price) / b
+                    supplies.append(supply(price))
+                    prices.append(price)
 
-    plt.figure()
-    plt.title(ru.MODEL_NAME)
-    plt.ylabel(ru.PRODUCT_PRICE)
-    plt.xlabel(ru.PRODUCT_NUMBER)
-    plt.plot(demands, prices)
-    plt.plot(supplies, prices)
+            elif t > b:
+                print(ru.UNWINDING_SPIRAL)
+                for k in range(5):
+                    demands.append(supply(price))
+                    price = (a - c - t * price) / b
+                    supplies.append(supply(price))
+                    prices.append(price)
+            else:
+                print(ru.TWISTING_SPIRAL)
+                while price != price_e:
+                    i += 1
+                    demands.append(supply(price))
+                    price = (a - c - t * price) / b
+                    supplies.append(supply(price))
+                    prices.append(price)
 
-    if price_begin != price_e:
-        plt.scatter(demands, prices)
-        plt.scatter(supplies, prices)
+            print(i, ru.NUMBER_CYCLES)
 
-        for m in demands:
-            for n in supplies:
-                if m == n:
-                    plt.quiver(n, prices[supplies.index(n)], 0, prices[demands.index(m)] - prices[supplies.index(n)],
+        plt.figure()
+        plt.title(ru.MODEL_NAME)
+        plt.ylabel(ru.PRODUCT_PRICE)
+        plt.xlabel(ru.PRODUCT_NUMBER)
+        plt.plot(demands, prices)
+        plt.plot(supplies, prices)
+
+        if price_begin != price_e:
+            plt.scatter(demands, prices)
+            plt.scatter(supplies, prices)
+
+            for m in demands:
+                for n in supplies:
+                    if m == n:
+                        plt.quiver(n, prices[supplies.index(n)], 0, prices[demands.index(m)] - prices[supplies.index(n)],
                                angles='xy', scale_units='xy', scale=1, width=0.005)
-        for q in range(len(prices)):
-            plt.quiver(demands[q], prices[q], supplies[q] - demands[q], 0,
-                       angles='xy', scale_units='xy', scale=1, width=0.005)
+            for q in range(len(prices)):
+                plt.quiver(demands[q], prices[q], supplies[q] - demands[q], 0,
+                            angles='xy', scale_units='xy', scale=1, width=0.005)
 
-    plt.scatter(quantity_e, price_e)
-    plt.show()
+        plt.scatter(quantity_e, price_e)
+        plt.show()
 
 
 if __name__ == '__main__':
